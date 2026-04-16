88975382b84aeafed2191b4a90a1c48b8c713505
zain9246hub <kureshizain04@gmail.com>
2026-04-16 19:28:14 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,33 +8,8 @@ if (!rootElement) {
   throw new Error("Root element not found");
 }
 
-class ErrorBoundary extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = { hasError: false, error: null };
-  }
-
-  static getDerivedStateFromError(error: any) {
-    return { hasError: true, error };
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <div style={{ padding: 20 }}>
-          <h1>🔥 App Crashed</h1>
-          <pre>{String(this.state.error)}</pre>
-        </div>
-      );
-    }
-    return this.props.children;
-  }
-}
-
 createRoot(rootElement).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>
 );
